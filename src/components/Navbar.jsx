3fe623dd48baa77a1ba343145460fd1118e14620
fthbng77 <fatihbing38@gmail.com>
2025-03-22 01:49:52 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
             <div className="flex justify-between items-center h-16">
                 <a href="#home" className="font-mono text-xl font-bold" text-white> 
                     {""}
-                    muaz<span className="text-blue-500">.lev</span>{""}
+                    Fatih<span className="text-blue-500">.bng</span>{""}
                 </a>
                 <div onClick={() => setMenuOpen((prev)=> !prev)} className="w-7 h-5 relative cursor-pointer z-40 md:hidden">
                     &#9776;
