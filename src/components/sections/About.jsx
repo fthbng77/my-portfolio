e6211ca6eb01aca1230bb37c5ea3a4001e4cd4a7
fthbng77 <fatihbing38@gmail.com>
2025-03-22 01:31:24 +0300

    update

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -2,28 +2,30 @@ import { RevealOnScroll } from "../RevealOnScroll";
 
 export const About = () => {
   const frontendSkills = [
-    "React + Vite",
-    "Angular",
-    "TypeScript",
-    "JavaScript",
-    "TailwindCSS",
-    "Bootstrap",
+    "ROS/ROS2",
+    "OpenCV",
+    "TensorFlow",
+    "PyTorch",
+    "Gazebo",
+    "Linux",
+    "Docker",
+    "SLAM",
+    "Image Processing",
+    "Raspberry Pi",
+    "NVIDIA Jetson",
+    "RGB-D, Stereo Cameras",
+    "Lidar"
   ];
 
-  const backendSkills = [".Net", "Asp.net/WebApi", "Sql", "Entity Framework"];
+  const backendSkills = [".Net", "NodeJS", "Sql", "Entity Framework"];
 
   const otherTechnologies = [
     "Python",
     "C++",
     "C#",
     "Java",
-    "Git",
-    "Linux",
-    "Ros",
-    "Gazebo",
-    "Ardupilot",
-    "Blender Software",
-  ];
+    "JavaScript"
+];
 
   return (
     <section
@@ -44,7 +46,7 @@ export const About = () => {
             </p>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+                <h3 className="text-xl font-bold mb-4"> Robotic Skills</h3>
                 <div className="flex flex-wrap gap-2">
                   {frontendSkills.map((tech, key) => (
                     <span
@@ -60,23 +62,7 @@ export const About = () => {
               </div>
 
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4"> Backend</h3>
-                <div className="flex flex-wrap gap-2">
-                  {backendSkills.map((tech, key) => (
-                    <span
-                      key={key}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
-                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
-                    "
-                    >
-                      {tech}
-                    </span>
-                  ))}
-                </div>
-              </div>
-
-              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4"> Other Technologies</h3>
+                <h3 className="text-xl font-bold mb-4"> Programming Skills</h3>
                 <div className="flex flex-wrap gap-2">
                   {otherTechnologies.map((tech, key) => (
                     <span
@@ -100,10 +86,6 @@ export const About = () => {
                 <li>
                   <strong> B.S. in Computer Engineering </strong> - Istanbul
                   University - Cerrahpasa (2021-2026)
-                </li>
-                <li>
-                  Relevant Coursework: Data Structures, Web Development, Cloud
-                  Computing...
                 </li>
               </ul>
             </div>
