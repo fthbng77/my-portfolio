0f6d7a7527f25ff23216d1ae6cc127d9123febe9
fthbng77 <fatihbing38@gmail.com>
2025-03-22 01:05:19 +0300

    update

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -94,7 +94,7 @@ export const Contact = () => {
                       d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                     />
                   </svg>
-                  <span>mhleventoglu@hotmail.com</span>
+                  <span>fth123bng@gmail.com</span>
                 </div>
 
                 <div className="flex items-center space-x-4">
@@ -112,7 +112,7 @@ export const Contact = () => {
                       d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
                     />
                   </svg>
-                  <span>Başakşehir, İstanbul, Türkiye</span>
+                  <span>Fatih, İstanbul, Türkiye</span>
                 </div>
 
                 <div className="flex items-center space-x-4">
@@ -130,14 +130,14 @@ export const Contact = () => {
                       d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                     />
                   </svg>
-                  <span>+123 456 7890</span>
+                  <span>+90 507 900 83 40</span>
                 </div>
               </div>
 
               {/* Social Media Links */}
               <div className="flex space-x-4">
                 <a
-                  href="https://github.com/MHLeventoglu"
+                  href="https://github.com/fthbng77"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="text-gray-400 hover:text-blue-500 transition"
@@ -152,7 +152,7 @@ export const Contact = () => {
                   </svg>
                 </a>
                 <a
-                  href="www.linkedin.com/in/muaz-hamza-leventoğlu-860109249"
+                  href="https://www.linkedin.com/in/fatih-bing%C3%B6l-1372b5222/"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="text-gray-400 hover:text-blue-500 transition"
